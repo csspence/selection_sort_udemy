081b0c17bbcbcb94f4b539962e9257f3e898f8fa
Crew Spence <csspence123@gmail.com>
2020-07-31 05:20:47 -0500

    Wrote working solution.

--- selection_sort_udemy.js
+++ selection_sort_udemy.js
@@ -3,6 +3,21 @@ JavaScript Algorithms and Data Structures Masterclass
 Selection Sort
 */
 
-const selctionSort = (arr) => {
-
+const selectionSort = (arr) => {
+  let min;
+  let temp;
+  for(let i = 0; i < arr.length; i++) {
+    min = i;
+    for(let j = i + 1; j < arr.length; j++) {
+      if(arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    if(min !== i) {
+      temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp; 
+    }
+  }
+  return arr;
 }
